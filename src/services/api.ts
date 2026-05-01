1d0f000e50ac5c6ac8bd0fee660181925fb99a91
ujval599 <ujvalbhavre99@gmail.com>
2026-05-01 14:53:18 +0530

    fix API URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,8 +3,7 @@
 
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000"
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: { "Content-Type": "application/json" },
